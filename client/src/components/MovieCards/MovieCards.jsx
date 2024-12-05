b31e4d3b91e147e94fb46ee19c225df57aca87eb
nicoleantoinette <nicole.antoinette.29@gmail.com>
2024-12-05 21:50:17 +0800

    added movie cards component and styles

--- client/src/components/MovieCards/MovieCards.jsx
+++ client/src/components/MovieCards/MovieCards.jsx
@@ -1,13 +1,12 @@
-import './MovieCards.css';
+import "./MovieCards.css";
 function MovieCards({ movie: movie, onClick }) {
   return (
     <>
-      <div className='card' onClick={onClick}>
+      <div className="card" onClick={onClick}>
         <img src={movie.posterPath} />
         <span>{movie.title}</span>
       </div>
     </>
   );
 }
-
 export default MovieCards;
